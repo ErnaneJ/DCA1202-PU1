016eb36efc2810a330f1cab6536da6d8d9db79ad
Ernane Ferreira <ernane.junior25@gmail.com>
2022-04-23 15:24:02 -0300

    Corrigindo equação da elipse tridimensional

--- questao_1/main.c
+++ questao_1/main.c
@@ -26,22 +26,16 @@ int ***allocate_array(int x, int y, int z){
 	return matriz;
 }
 
-void set_data_matriz(int ***matriz, int x, int y, int z, int a, int b){
-  int cpx=0, cpy=0;
-
+void set_data_matriz(int ***matriz, int x, int y, int z, int a, int b, int r_c){
   for(int i = 0; i < x; i++){
     for(int j = 0; j < y; j++){
       for(int k = 0; k < z; k++){
-        int condition = 1 > (pow((i-x/2), 2)/pow(a,2) + pow((j-y/2), 2)/pow(b,2));
+        int condition = 1 >= (pow((i-(x/2)), 2)/pow(a,2) + pow((j-(y/2)), 2)/pow(b,2) + pow(k-(z/2), 2)/pow(r_c,2));
 
-        // batman? HAHA.. => (a > b)
-        // int connditionA = pow(a, 2) > (pow((i-x/2), 2) + pow((j-y/2), 2));
-        // int connditionB = pow(b, 2) > (pow((i-x/2), 2) - pow((j-y/2), 2));
-        // if(connditionA && connditionB){
         if(condition){
           matriz[i][j][k] = 1;
         }else{
-          matriz[i][j][k] = 0;          
+          matriz[i][j][k] = 0; 
         }
       }
     }
@@ -57,7 +51,7 @@ void print_array(int ***matriz, int x, int y, int plan_z){
   }
 }
 int main(){
-  int ***matriz, x=30, y=30, z=30, r_a=9, r_b=13, plan_z=4; // Valores default para testes;
+  int ***matriz, x=20, y=20, z=20, r_a=6, r_b=8, r_c=5, plan_z=0, show_all = 0; // Valores default para testes;
   system("@cls||clear");
   printf("=====| Dimensões da matriz: \n");
   printf(".:: Quantidade de elementos na direção de X: ");
@@ -68,19 +62,38 @@ int main(){
   scanf("%d", &z);
 
   printf("\n=====| Raios da elipse: \n");
-  printf(".:: Digite raio menor (a) da elipse: ");
+  printf(".:: Digite raio \"a\" da elipse: ");
   scanf("%d", &r_a);
-  printf(".:: Digite o raio maior (b) da elipse: ");
+  printf(".:: Digite o raio \"b\" da elipse: ");
   scanf("%d", &r_b);
+  printf(".:: Digite o raio \"c\" da elipse: ");
+  scanf("%d", &r_c);
+
+  matriz = allocate_array(x, y, z);
+  set_data_matriz(matriz, x, y, z, r_a, r_b, r_c);
 
   printf("\n=====| Seção de corte transversal no plano z: \n");
-  printf(".:: Qual dimensão (z) deseja mostrar a elipse 0-%d? ", z-1);
-  scanf("%d", &plan_z);
-
+  printf(".:: Deseja, sim(1) ou não(0), mostrar todos os cortes disponíveis? ");
+  scanf("%d", &show_all);
   printf("\n");
-	matriz = allocate_array(x, y, z);
-  set_data_matriz(matriz, x, y, z, r_a, r_b);
-
-  print_array(matriz, x, y, plan_z);
+  if(show_all == 1){
+    for(int i = 0; i<z; i++){
+      print_array(matriz, x, y, i);
+      printf("\n");
+    }
+  }
+  while (plan_z >= 0){
+    printf("\n.:: Qual seção de corte em z deseja visualizar?\n");
+    printf(".:: Digite -1 para sair ou a seção de corte desejada entre 0 e %d: ", z-1);
+    scanf("%d", &plan_z);
+    while (plan_z >= z){
+      printf("\n.:: Atenção! A seção indicada não está definida.\n");
+      printf(".:: Digite -1 para sair ou a seção de corte desejada entre 0 e %d: ", z-1);
+      scanf("%d", &plan_z);
+    }
+    if(plan_z < 0) exit(1);
+    print_array(matriz, x, y, plan_z);
+  }
+  
   return 0;
 }
