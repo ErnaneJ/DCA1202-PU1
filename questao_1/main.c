bf7dd468c435ac2dcc6b29162b5fb375420020da
Ernane Ferreira <ernane.junior25@gmail.com>
2022-05-15 23:08:00 -0300

    Ajustando pontos importantes e documentado decisões

--- questao_1/main.c
+++ questao_1/main.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 #include <math.h>
 
-int ***allocate_array(int x, int y, int z){
+int ***allocate_array(int x, int y, int z){ // Aloca dinamicamente a matriz de dimensões x, y e z.
 	int ***matriz = (int***)malloc(x * sizeof(int**));
  	if (matriz == NULL){
-		printf("\n\n> Erro ao realizar alocacao.\n> Programa encerrado.\n\n");
+		printf("\n\n> Erro ao realizar alocação.\n> Programa encerrado.\n\n");
     exit(1);
 	}
   for (int i = 0; i < x; i++){
     matriz[i] = (int**)malloc(y * sizeof(int*));
     if (matriz[i] == NULL){
-      printf("\n\n> Erro ao realizar alocacao.\n> Programa encerrado.\n\n");
+      printf("\n\n> Erro ao realizar alocação.\n> Programa encerrado.\n\n");
     	exit(1);
     }
     for (int j = 0; j < y; j++)    {
       matriz[i][j] = (int*)malloc(z * sizeof(int));
       if (matriz[i][j] == NULL){
-				printf("\n\n> Erro ao realizar alocacao.\n> Programa encerrado.\n\n");
+				printf("\n\n> Erro ao realizar alocação.\n> Programa encerrado.\n\n");
     		exit(1);
       }
     }
@@ -26,13 +26,13 @@ int ***allocate_array(int x, int y, int z){
 	return matriz;
 }
 
-void set_data_matriz(int ***matriz, int x, int y, int z, int a, int b, int r_c){
+// Insere na matriz os dados correspondentes ao elipsoide
+void set_data_matriz(int ***matriz, int x, int y, int z, int a, int b, int r_c){ 
   for(int i = 0; i < x; i++){
     for(int j = 0; j < y; j++){
       for(int k = 0; k < z; k++){
-        int condition = 1 >= (pow((i-(x/2)), 2)/pow(a,2) + pow((j-(y/2)), 2)/pow(b,2) + pow(k-(z/2), 2)/pow(r_c,2));
-
-        if(condition){
+        // Verifica se o ponto da iteração atual esta incluso no elipsoide
+        if(1 >= (pow((i-(x/2)), 2)/pow(a,2) + pow((j-(y/2)), 2)/pow(b,2) + pow(k-(z/2), 2)/pow(r_c,2))){
           matriz[i][j][k] = 1;
         }else{
           matriz[i][j][k] = 0; 
@@ -42,7 +42,7 @@ void set_data_matriz(int ***matriz, int x, int y, int z, int a, int b, int r_c){
   }
 }
 
-void print_array(int ***matriz, int x, int y, int plan_z){
+void print_array(int ***matriz, int x, int y, int plan_z){ // Imprime um determinado plano da matriz
   for (int i = 0; i < x; i++){
     for (int j = 0; j < y; j++){
       printf("%d ", matriz[i][j][plan_z]);
@@ -50,8 +50,10 @@ void print_array(int ***matriz, int x, int y, int plan_z){
     printf("\n");
   }
 }
+
 int main(){
-  int ***matriz, x=20, y=20, z=20, r_a=6, r_b=8, r_c=5, plan_z=0, show_all = 0; // Valores default para testes;
+  // Valores inicializados apenas para facilitar testes
+  int ***matriz, x=20, y=20, z=20, r_a=6, r_b=8, r_c=5, plan_z=0, show_all = 0;
   system("@cls||clear");
   printf("=====| Dimensões da matriz: \n");
   printf(".:: Quantidade de elementos na direção de X: ");
@@ -62,11 +64,11 @@ int main(){
   scanf("%d", &z);
 
   printf("\n=====| Raios da elipse: \n");
-  printf(".:: Digite raio \"a\" da elipse: ");
+  printf(".:: Digite raio (até %d) \"a\" da elipse: ", x/2);
   scanf("%d", &r_a);
-  printf(".:: Digite o raio \"b\" da elipse: ");
+  printf(".:: Digite o raio (até %d) \"b\" da elipse: ", y/2);
   scanf("%d", &r_b);
-  printf(".:: Digite o raio \"c\" da elipse: ");
+  printf(".:: Digite o raio (até %d) \"c\" da elipse: ", z/2);
   scanf("%d", &r_c);
 
   matriz = allocate_array(x, y, z);
@@ -76,24 +78,38 @@ int main(){
   printf(".:: Deseja, sim(1) ou não(0), mostrar todos os cortes disponíveis? ");
   scanf("%d", &show_all);
   printf("\n");
-  if(show_all == 1){
+
+  if(show_all == 1){ // Imprime todos os planos disponíveis caso desejado
     for(int i = 0; i<z; i++){
       print_array(matriz, x, y, i);
       printf("\n");
     }
   }
-  while (plan_z >= 0){
+
+  while (plan_z >= 0){ 
     printf("\n.:: Qual seção de corte em z deseja visualizar?\n");
     printf(".:: Digite -1 para sair ou a seção de corte desejada entre 0 e %d: ", z-1);
     scanf("%d", &plan_z);
-    while (plan_z >= z){
+    while (plan_z >= z){ // Caso o usuario coloque um plano não permitido solicitará a ele um novo
       printf("\n.:: Atenção! A seção indicada não está definida.\n");
       printf(".:: Digite -1 para sair ou a seção de corte desejada entre 0 e %d: ", z-1);
       scanf("%d", &plan_z);
     }
-    if(plan_z < 0) exit(1);
+    if(plan_z < 0) exit(1); // Se a entrada for negativa, o usuário deseja encerrar o programa
     print_array(matriz, x, y, plan_z);
   }
+
+  // Libera matriz
+  for (int i = 0; i < x; i++){
+    for (int j = 0; j < y; j++){
+      free(matriz[i][j]);
+      matriz[i][j] = NULL;
+    }
+    free(matriz[i]);
+    matriz[i] = NULL;
+  }
+  free(matriz);
+  matriz = NULL;
   
   return 0;
 }
--- questao_2/main.c
+++ questao_2/main.c
@@ -1,5 +1,5 @@
 /*
-* Para mudar o estado de um determinado bit n podemos seguir dois possíveis, simples, caminhos: 
+* Para mudar o estado de um determinado bit n podemos seguir dois possíveis, caminhos: 
 * realizar um OR bit a bit (|) entre a variável a ser alterada e valor 0B00000001000000000000000000000000 
 * sendo 1 apenas o o bit que queremos que seja setado, sesse caso bit 24. Ou, para zerarmos, esse mesmo bit 24, 
 * podemos realizar um AND bit a bit (&) entre a variàvel desejada e o valor 0B11111110111111111111111111111111 sendo apenas
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-void printbyte(unsigned char byte){
+void printbyte(unsigned char byte){ // imprime um determinado byte
   unsigned char i;
   for(i = 128 ; i > 0 ; i = i >> 1){
     if(i & byte){ printf("1"); }
@@ -18,7 +18,7 @@ void printbyte(unsigned char byte){
   }
 }
 
-void set_data_array(unsigned char array[][8]){
+void set_data_array(unsigned char array[][8]){ // popula array com valores aleatórios entre 0 e 1
   srand(time(NULL));
   for(int i = 0; i < 8; i++){
     for(int j = 0; j < 8; j++){
@@ -27,7 +27,7 @@ void set_data_array(unsigned char array[][8]){
   }
 }
 
-void print_array(unsigned char array[][8], int show_inline){
+void print_array(unsigned char array[][8], int show_inline){ // imprime um determinado array
   if(show_inline == 0){
     printf("    ");
     for (int i = 0; i < 8; i++){
@@ -46,29 +46,32 @@ void print_array(unsigned char array[][8], int show_inline){
   }
 }
 
-void operating_bit_by_bit(unsigned char bit_array[][8], int show_operations, unsigned char *p_array){
+// realiza operações bit a bit
+void operating_bit_by_bit(unsigned char bit_array[][8], int show_operations, unsigned char *p_array){ 
   unsigned char i;
   int count_colunms = 0, count_lines = 0;
   for(int k=7; k>=0; k--){
     count_colunms = 0;
     for(i = 128 ; i > 0 ; i = i >> 1){
       if(bit_array[count_lines][count_colunms] == 0){
-        if(show_operations){
+        if(show_operations){ // Mosta a operação caso solicitado
           printf("Envia sinal 1 para o bit %d do byte %d: \n", i, k);
           printf("  ");printbyte(~i);printf("\n  ");
           printbyte(p_array[k]);printf("  AND(&) \n");
           printf("-------------------\n  ");
+          // Realiza AND bit a bit entre o valor atual de p_array e o byte atual i que possui apenas 1 bit sinalizado como 1
           p_array[k] &= ~i;
-          printbyte(p_array[k]);printf("\n\n");
+          printbyte(p_array[k]);printf("\n\n"); // mostra resultado da operação
         }else{p_array[k] &= ~i;}
       }else{
-        if(show_operations){
+        if(show_operations){ // Mosta a operação caso solicitado
           printf("Envia sinal 0 para o bit %d do byte %d: \n", i, k);
           printf("  ");printbyte(i);printf("\n  ");
           printbyte(p_array[k]);printf("  OR(|) \n");
           printf("-------------------\n  ");
+          // Realiza OR bit a bit entre o valor atual de p_array e o byte atual i que possui apenas 1 bit sinalizado como 0
           p_array[k] |= i;  
-          printbyte(p_array[k]);printf("\n\n");
+          printbyte(p_array[k]);printf("\n\n"); // mostra resultado da operação
         }else{p_array[k] |= i;}
       }
       count_colunms++;
@@ -79,8 +82,8 @@ void operating_bit_by_bit(unsigned char bit_array[][8], int show_operations, uns
 
 int main(){
   unsigned long int inline_bits;
-  unsigned char bit_array[8][8];
-  unsigned char *p_array;
+  unsigned char bit_array[8][8], *p_array;
+  
   int count_lines = 0, count_colunm = 0, show_operations = 0;
 
   p_array = &inline_bits;
@@ -88,19 +91,20 @@ int main(){
   set_data_array(bit_array);
 
   printf("\n.:: Array de sinais 8x8:\n\n");
-  print_array(bit_array, 0);
+  print_array(bit_array, 0); // Mostra matriz de dados gerada
 
   printf("\n.:: Populando unsigned long int com sinais do array 2D 8x8..\n");
   printf("\n.:: Deseja, Sim(1) ou Não(1), mostrar as operações bit a bit? ");
   scanf("%d", &show_operations);printf("\n");
 
+  // Opera bit a bit para popular o inteiro não sinalizado de 64 bits com os dados entre 0 e 1 da matriz
   operating_bit_by_bit(bit_array, show_operations, p_array);
 
   printf(".:: Comparativo: ");
-  printf("\n.::: Array de sinais 8x8 em linha: |");
+  printf("\n.::: Array de sinais 8x8 em linha:   |");
   print_array(bit_array, 1);
   
-  printf("\n.::: Bits do Unsigned long int:    |");
+  printf("\n.::: Bits do inteiro não sinalizado: |");
   for(int i=7; i>=0; i--){
     printbyte(p_array[i]);
     printf("|");
